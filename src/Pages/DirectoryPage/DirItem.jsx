52f2b477608fda827b9b1d59bf4fcc31932ed22e
Oikapi <seryy.gordeev.78@mail.ru>
2024-01-30 17:24:44 +0700

    mobile2

--- src/Pages/DirectoryPage/DirItem.jsx
+++ src/Pages/DirectoryPage/DirItem.jsx
@@ -9,7 +9,7 @@ const DirItem = ({ element }) => {
     return (
         <div className="dir-item-container">
             <div className="dir-item-img"><img src={element.pic} alt="" /></div>
-            <div className="dir-item-text">{element.answer}</div>
+            <div className="dir-item-text"><span>{element.answer}</span></div>
         </div>
     )
 }
