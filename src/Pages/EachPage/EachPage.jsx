847a2765a2301113e64cd9ce981444ec61e38c76
Oikapi <seryy.gordeev.78@mail.ru>
2024-03-17 16:35:24 +0700

    add sign

--- src/Pages/EachPage/EachPage.jsx
+++ src/Pages/EachPage/EachPage.jsx
@@ -9,17 +9,29 @@ const EachPage = () => {
     const alreadyDone = useSelector(state => state.alreadyDone)
     console.log(question)
     return (
-        <div className='start-component'>
-            <button className="start-button" onClick={() => navigate("/test")}>
-                НАЧАТЬ ТЕСТ
-            </button>
-            <button className="start-button" onClick={() => navigate("/directory")}>
-                СПРАВОЧНИК
-            </button>
-            <button className="start-button" onClick={() => navigate("/cards")}>
-                КАРТОЧКИ
-            </button>
-        </div>
+        <>
+            <div className='start-component'>
+                <button className="start-button" onClick={() => navigate("/test")}>
+                    НАЧАТЬ ТЕСТ
+                </button>
+                <button className="start-button" onClick={() => navigate("/directory")}>
+                    СПРАВОЧНИК
+                </button>
+                <button className="start-button" onClick={() => navigate("/cards")}>
+                    КАРТОЧКИ
+                </button>
+                <div className='sign'>
+                    <p>Преподаватель:<br />
+                        Петрова Людмила Генадьевна<br />
+                        Студенты:<br />
+                        Березовский Егор Дмитриевич<br />
+                        Батов Илья Ильич<br />
+                        Васильев Даниил Евгеньевич<br />
+                    </p>
+                </div>
+            </div>
+
+        </>
     )
 }
 
